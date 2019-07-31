c569bccf647807918ea61409d765b04204e97ab2
Gurwinder Singh <vargwin@gmail.com>
2019-08-01 01:37:39 +0530

    Update tests/server.rs, tests/trailers.rs

--- tests/h2-tests/tests/trailers.rs
+++ tests/h2-tests/tests/trailers.rs
@@ -1,7 +1,7 @@
 #![feature(async_await)]
 
-use h2_support::prelude::*;
 use futures::StreamExt;
+use h2_support::prelude::*;
 
 #[tokio::test]
 async fn recv_trailers_only() {
@@ -11,14 +11,14 @@ async fn recv_trailers_only() {
         .handshake()
         // Write GET /
         .write(&[
-            0, 0, 0x10, 1, 5, 0, 0, 0, 1, 0x82, 0x87, 0x41, 0x8B, 0x9D, 0x29,
-                0xAC, 0x4B, 0x8F, 0xA8, 0xE9, 0x19, 0x97, 0x21, 0xE9, 0x84,
+            0, 0, 0x10, 1, 5, 0, 0, 0, 1, 0x82, 0x87, 0x41, 0x8B, 0x9D, 0x29, 0xAC, 0x4B, 0x8F,
+            0xA8, 0xE9, 0x19, 0x97, 0x21, 0xE9, 0x84,
         ])
         .write(frames::SETTINGS_ACK)
         // Read response
         .read(&[
-              0, 0, 1, 1, 4, 0, 0, 0, 1, 0x88, 0, 0, 9, 1, 5, 0, 0, 0, 1,
-              0x40, 0x84, 0x42, 0x46, 0x9B, 0x51, 0x82, 0x3F, 0x5F,
+            0, 0, 1, 1, 4, 0, 0, 0, 1, 0x88, 0, 0, 9, 1, 5, 0, 0, 0, 1, 0x40, 0x84, 0x42, 0x46,
+            0x9B, 0x51, 0x82, 0x3F, 0x5F,
         ])
         .build();
 
@@ -42,7 +42,10 @@ async fn recv_trailers_only() {
     let chunk = h2.run(Box::pin(async { body.next().await }));
     assert!(chunk.is_none());
 
-    let trailers = h2.run(poll_fn(|cx| body.poll_trailers(cx))).unwrap().unwrap();
+    let trailers = h2
+        .run(poll_fn(|cx| body.poll_trailers(cx)))
+        .unwrap()
+        .unwrap();
     assert_eq!(1, trailers.len());
     assert_eq!(trailers["status"], "ok");
 
@@ -57,16 +60,15 @@ async fn send_trailers_immediately() {
         .handshake()
         // Write GET /
         .write(&[
-               0, 0, 0x10, 1, 4, 0, 0, 0, 1, 0x82, 0x87, 0x41, 0x8B, 0x9D, 0x29,
-               0xAC, 0x4B, 0x8F, 0xA8, 0xE9, 0x19, 0x97, 0x21, 0xE9, 0x84, 0, 0,
-               0x0A, 1, 5, 0, 0, 0, 1, 0x40, 0x83, 0xF6, 0x7A, 0x66, 0x84, 0x9C,
-               0xB4, 0x50, 0x7F,
+            0, 0, 0x10, 1, 4, 0, 0, 0, 1, 0x82, 0x87, 0x41, 0x8B, 0x9D, 0x29, 0xAC, 0x4B, 0x8F,
+            0xA8, 0xE9, 0x19, 0x97, 0x21, 0xE9, 0x84, 0, 0, 0x0A, 1, 5, 0, 0, 0, 1, 0x40, 0x83,
+            0xF6, 0x7A, 0x66, 0x84, 0x9C, 0xB4, 0x50, 0x7F,
         ])
         .write(frames::SETTINGS_ACK)
         // Read response
         .read(&[
-               0, 0, 1, 1, 4, 0, 0, 0, 1, 0x88, 0, 0, 0x0B, 0, 1, 0, 0, 0, 1,
-               0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x77, 0x6F, 0x72, 0x6C, 0x64,
+            0, 0, 1, 1, 4, 0, 0, 0, 1, 0x88, 0, 0, 0x0B, 0, 1, 0, 0, 0, 1, 0x68, 0x65, 0x6C, 0x6C,
+            0x6F, 0x20, 0x77, 0x6F, 0x72, 0x6C, 0x64,
         ])
         .build();
 
@@ -92,7 +94,10 @@ async fn send_trailers_immediately() {
     let (_, mut body) = response.into_parts();
 
     // There is a data chunk
-    let _ = h2.run(Box::pin(async { body.next().await })).unwrap().unwrap();
+    let _ = h2
+        .run(Box::pin(async { body.next().await }))
+        .unwrap()
+        .unwrap();
 
     let chunk = h2.run(Box::pin(async { body.next().await }));
     assert!(chunk.is_none());
